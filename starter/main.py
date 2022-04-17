9d265e94c78ce4faff372d668a7f111496e7488f
Stefan Popov <popovstefan@live.com>
2022-04-17 13:05:33 +0200

    Modified import paths

--- starter/main.py
+++ starter/main.py
@@ -9,12 +9,11 @@ import os
 import joblib
 import pandas as pd
 from fastapi import FastAPI
-
-from person_model import Person
-from starter import train_model
-from starter.ml.data import process_data
-from starter.ml import model
-
+from typing import Literal
+from .starter import train_model
+from .starter.ml.data import process_data
+from .starter.ml import model
+from pydantic.main import BaseModel
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
@@ -23,6 +22,124 @@ if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 app = FastAPI()
+
+
+class Person(BaseModel):
+    class Config:
+        schema_extra = {
+            "example": {
+                "age": 45,
+                "workclass": "State-gov",
+                "education": "HS-grad",
+                "maritalStatus": "Divorced",
+                "occupation": "Protective-serv",
+                "relationship": "Unmarried",
+                "race": "Black",
+                "sex": "Female",
+                "hoursPerWeek": 40,
+                "nativeCountry": "United-States"
+            }
+        }
+
+    age: int
+    workclass: Literal['State-gov',
+                       'Self-emp-not-inc',
+                       'Private',
+                       'Federal-gov',
+                       'Local-gov',
+                       'Self-emp-inc',
+                       'Without-pay']
+    education: Literal['Bachelors',
+                       'HS-grad',
+                       '11th',
+                       'Masters',
+                       '9th',
+                       'Some-college',
+                       'Assoc-acdm',
+                       '7th-8th',
+                       'Doctorate',
+                       'Assoc-voc',
+                       'Prof-school',
+                       '5th-6th',
+                       '10th',
+                       'Preschool',
+                       '12th',
+                       '1st-4th']
+    maritalStatus: Literal['Never-married',
+                           'Married-civ-spouse',
+                           'Divorced',
+                           'Married-spouse-absent',
+                           'Separated',
+                           'Married-AF-spouse',
+                           'Widowed']
+    occupation: Literal['Adm-clerical',
+                        'Exec-managerial',
+                        'Handlers-cleaners',
+                        'Prof-specialty',
+                        'Other-service',
+                        'Sales',
+                        'Transport-moving',
+                        'Farming-fishing',
+                        'Machine-op-inspct',
+                        'Tech-support',
+                        'Craft-repair',
+                        'Protective-serv',
+                        'Armed-Forces',
+                        'Priv-house-serv']
+    relationship: Literal['Not-in-family',
+                          'Husband',
+                          'Wife',
+                          'Own-child',
+                          'Unmarried',
+                          'Other-relative']
+    race: Literal['White',
+                  'Black',
+                  'Asian-Pac-Islander',
+                  'Amer-Indian-Eskimo',
+                  'Other']
+    sex: Literal['Male', 'Female']
+    hoursPerWeek: int
+    nativeCountry: Literal['United-States',
+                           'Cuba',
+                           'Jamaica',
+                           'India',
+                           'Mexico',
+                           'Puerto-Rico',
+                           'Honduras',
+                           'England',
+                           'Canada',
+                           'Germany',
+                           'Iran',
+                           'Philippines',
+                           'Poland',
+                           'Columbia',
+                           'Cambodia',
+                           'Thailand',
+                           'Ecuador',
+                           'Laos',
+                           'Taiwan',
+                           'Haiti',
+                           'Portugal',
+                           'Dominican-Republic',
+                           'El-Salvador',
+                           'France',
+                           'Guatemala',
+                           'Italy',
+                           'China',
+                           'South',
+                           'Japan',
+                           'Yugoslavia',
+                           'Peru',
+                           'Outlying-US(Guam-USVI-etc)',
+                           'Scotland',
+                           'Trinadad&Tobago',
+                           'Greece',
+                           'Nicaragua',
+                           'Vietnam',
+                           'Hong',
+                           'Ireland',
+                           'Hungary',
+                           'Holand-Netherlands']
 
 
 @app.get("/")
