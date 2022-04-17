c9070781068a0e7b53249260d3be9a16d3cbe9c4
Stefan Popov <popovstefan@live.com>
2022-04-17 13:47:28 +0200

    Update main.py

--- starter/main.py
+++ starter/main.py
@@ -17,7 +17,7 @@ from pydantic.main import BaseModel
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
+    if os.system("dvc pull -R") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
